3a6a12f652ac92a20a66b3f3f871c234f0792037
Student <user@chromebox-004.(none)>
2013-04-30 19:48:25 -0700

    adding postgres module and pulling data from google calendar with google-calendar module

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(express.cookieParser());
+app.use(express.session({
+	secret: "skjghskdjfhbqigohqdiouk"
+}));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -24,11 +28,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 // development only
 if ('development' == app.get('env')) {
  app.use(express.errorHandler());
+ app.set('db connection', "tcp://postgres:5432@localhost/postgres");
 }
 
-app.get('/', routes.index);
-app.get('/calendar/search_calendar', calendar.searchCalendar);
-app.all('/authentication', calendar.googleCalendar);
+// for production
+if ('production' == app.get('env')) {
+	app.set('db connection', process.env.DATABASE_URL);
+}
+
+app.all('/', calendar.google_authenticate);
+app.all('/calendarlist', calendar.calendar_list);
 
 http.createServer(app).listen(app.get('port'), function(){
  console.log('Express server listening on port ' + app.get('port'));
--- tables.sql
+++ tables.sql
@@ -1,18 +1,25 @@
 Tables
 
-CREATE TABLE appts (
-    id serial CONSTRAINT appt_id_pk PRIMARY KEY,
-    apptName text NOT NULL,
-    startDate date NOT NULL,
-    endDate date NOT NULL,
-    weekendsOnly boolean NOT NULL,
-    startTime time NOT NULL,
-    endTime time NOT NULL,
-    apptDuration integer NOT NULL
+-- CREATE TABLE appts (
+--     id serial CONSTRAINT appt_id_pk PRIMARY KEY,
+--     apptName text NOT NULL,
+--     startDate date NOT NULL,
+--     endDate date NOT NULL,
+--     weekendsOnly boolean NOT NULL,
+--     startTime time NOT NULL,
+--     endTime time NOT NULL,
+--     apptDuration integer NOT NULL
+-- );
+
+
+-- INSERT INTO events (apptName, startDate, endDate, weekendsOnly, startTime, endTime, apptDuration) VALUES
+--     ('Lunch', '3/22/2013', '3/30/2013', 'on', '01:30PM', '03:30PM', 1),
+--     ('Dinner', '4/22/2013', '4/30/2013', 'on', '06:30PM', '08:30PM', 1),
+--     ('Breakfast', '5/22/2013', '5/30/2013', 'on', '09:30AM', '11:30AM', 1),
+
+
+CREATE TABLE users (
+    id serial CONSTRAINT user_id_pk PRIMARY KEY,
+    access_token varchar,
+    email varchar
 );
-
-
-INSERT INTO events (apptName, startDate, endDate, weekendsOnly, startTime, endTime, apptDuration) VALUES
-    ('Lunch', '3/22/2013', '3/30/2013', 'on', '01:30PM', '03:30PM', 1),
-    ('Dinner', '4/22/2013', '4/30/2013', 'on', '06:30PM', '08:30PM', 1),
-    ('Breakfast', '5/22/2013', '5/30/2013', 'on', '09:30AM', '11:30AM', 1),
